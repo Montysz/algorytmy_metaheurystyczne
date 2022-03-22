1ea179c724fdcb32011f17df880d6be1ef97f755
Pawel <254642@student.pwr.edu.pl>
2022-03-22 17:03:34 +0100

    huge update

--- main.py
+++ main.py
@@ -19,16 +19,42 @@ def read(path):
     return problem
 
 def random_instance(n, seed, type, a = 2, b = 100):
-    G = nx.Graph()
+    G = None
     seed = seed^(a+b)^n
     random.seed(seed)
 
-    if type == "not_dirested":
+    if type == "Symmetric":
+        G = nx.Graph()
         for i in range(1,n+1):
             for j in range(i + 1,n+1):
                 w = random.randint(a,b)
                 G.add_edge(i, j, weight = w)
- 
+    
+    if type == "EUC_2D":
+        G = nx.Graph()
+        s = """ NAME : ramdominstance
+                COMMENT : .
+                TYPE : TSP
+                DIMENSION : 100
+                EDGE_WEIGHT_TYPE : EUC_2D
+                NODE_COORD_SECTION
+                """
+        for i in range(1, n+1):
+            x = random.randint(a,b)
+            y = random.randint(a,b)
+            s = s + f"{i} {x} {y} \n"
+        s = s + "EOF\n"
+        problem = tsplib95.parse(s)   
+        G = problem.get_graph() 
+        
+    if type == "Asymmetric":
+        G = nx.DiGraph()
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                w = random.randint(a,b)
+                G.add_edge(i, j, weight = w)
+
+    
                      
     return G
 
@@ -37,15 +63,21 @@ def distance_matrix(graph):
     #for i in x:
     #    print(i)
 
-def solution_print(graph, tour):
+def solution_print(graph, tour, path='here.png'):
     edgelist = []
     
-    for i in range(len(tour) - 1):
-        edgelist.append((tour[i], tour[i + 1]))
-    edgelist.append((tour[len(tour) - 1], tour[0]))
+    if p.edge_weight_format == "FULL_MATRIX":
+        for i in range(len(tour) - 1):
+            edgelist.append((tour[i]-1, tour[i + 1]-1))
+        edgelist.append( (tour[len(tour) - 1]-1, tour[0]-1) )
+    else:
+        for i in range(len(tour) - 1):
+            edgelist.append((tour[i], tour[i + 1]))
+        edgelist.append( (tour[len(tour) - 1], tour[0]) )
     print(edgelist)
     n = nx.get_node_attributes(graph, 'coord')
     l = len(n)
+    
 
     if l > 0 and n[1] != None:
 
@@ -54,8 +86,9 @@ def solution_print(graph, tour):
         pos = nx.spring_layout(graph, seed=7, ) 
         nx.draw_networkx_nodes(graph, pos, node_size=300, node_color = 'green')
         nx.draw_networkx_edges(graph, pos, edgelist=edgelist, width=2)
-    plt.savefig('here.png') 
-    plt.show()
+    plt.savefig(path) 
+    #plt.show()
+    plt.close()
 
 def graph_print(graph):
     n = nx.get_node_attributes(graph, 'coord')
@@ -83,7 +116,7 @@ def evaluate(graph, tour):
     return sum
 
 def prd(graph, x, ref):
-    print(100 * ((evaluate(graph, x) - ref) / ref),"%")
+    print(f"{100*((evaluate(graph,x)-ref)/ref)}%")
 
 def tests():
     import os
@@ -129,10 +162,9 @@ def nearest_neighbour(G, s):
     solution.append(s)
     while(notYetVisited(visited)):
         curEdges =  [(Graph[s-1][i],i+1) for i in range(len(Graph[0]))]
-
         sortedEdges = sorted(curEdges, key=lambda k: k[0])
         
-        for i in range(1, len(Graph[0])+1):
+        for i in range(0, len(Graph[0])):
             if not visited[sortedEdges[i][1]]:
                 visited[sortedEdges[i][1]] = True
                 solution.append(sortedEdges[i][1])
@@ -142,44 +174,92 @@ def nearest_neighbour(G, s):
 def nearest_neighbour_extended(G):
     Graph = distance_matrix(G)
     best_sol = 1e9
+    best_ans = None
     for s in range(1, len(Graph[0]) + 1):
-        visited = [0 for i in range(len(Graph[0])+1)]
-        visited[s] = 1
-        visited[0] = 1
-        best_ans = None
-        solution = []
-        solution.append(s)
-        while(notYetVisited(visited)):
-            curEdges =  [(Graph[s-1][i],i+1) for i in range(len(Graph[0]))]
-
-            sortedEdges = sorted(curEdges, key=lambda k: k[0])
-            
-            for i in range(1, len(Graph[0])+1):
-                if not visited[sortedEdges[i][1]]:
-                    visited[sortedEdges[i][1]] = True
-                    solution.append(sortedEdges[i][1])
-                    break
+        solution = nearest_neighbour(G, s)
         cur_sol = evaluate(G, solution)
         if cur_sol < best_sol:
             best_sol = cur_sol
             best_ans = solution
-            print(best_sol)
     return best_ans
 
+def cost(cost_mat, route):
+    
+    
+    ans = cost_mat[np.roll(route, 1), route].sum()
+    
+    return ans
 
-name = "berlin52"
+def two_opt(G, route):
+    best = route
+    for i in range(len(best)):
+        best[i] = best[i] - 1
+    Graph = distance_matrix(G)   
+    improved = True
+    while improved:
+        improved = False
+        for i in range(1, len(route)):
+            for j in range(i+2, len(route)):
+                new_route = route.copy()
+                new_route[i:j] = route[j-1:i-1:-1] # this is the 2woptSwap
+                if cost(Graph, new_route) < cost(Graph, best):  # what should cost be?
+                    best = new_route
+                    improved = True
+        route = best
+    for i in range(len(best)):
+        best[i] = best[i] + 1
+    return best
+
+
+name = "st70"
 p = read("tsp/"+str(name)+".tsp")
 G = p.get_graph()
 
 t = read("tsp/"+str(name)+".opt.tour")
 opt = t.tours[0]
 
-kr = kRandom(G, 2)[1]
-n = nearest_neighbour(G, 5)
+
+
+kr = kRandom(G, 100)[1]
+print(evaluate(G,kr))
+
+n = nearest_neighbour(G, 2)
+print(evaluate(G,n))
+
 ne = nearest_neighbour_extended(G)
-#solution_print(G,n)
+print(evaluate(G,ne))
 
-prd(G, kr, evaluate(G,opt))
-prd(G, n, evaluate(G,opt))
-prd(G, ne, evaluate(G,opt))
+to = two_opt(G, kr.copy())
+print(evaluate(G,to))
 
+solution_print(G, kr, 'kr.png')
+solution_print(G, n, 'n.png')
+solution_print(G, ne, 'ne.png')
+solution_print(G, to, 'to.png')
+'''
+#    random_instance(n, seed, type, a = 2, b = 100):
+#    Symmetric
+#    EUC_2D
+#    Asymmetric
+
+G  = random_instance(32, 2138, "Symmetric")
+
+
+kr = kRandom(G, 100)[1]
+print(evaluate(G, kr))
+
+n = nearest_neighbour(G, 2)
+print(evaluate(G,n))
+
+ne = nearest_neighbour_extended(G)
+print(evaluate(G,ne))
+
+to = two_opt(G, kr.copy())
+print(evaluate(G,to))
+
+solution_print(G, kr, 'kr.png')
+solution_print(G, n, 'n.png')
+solution_print(G, ne, 'ne.png')
+solution_print(G, to, 'to.png')
+
+'''
